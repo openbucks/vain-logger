b20ce4ecb3e9dcaaa43960de055cde824f6489c8
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:27:27 +0300

    Added di.yml

---
+++ src/Extension/LoggerExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-logger
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-logger
+ */
+namespace Vain\Logger\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class LoggerExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class LoggerExtension extends AbstractExtension
+{
+}
